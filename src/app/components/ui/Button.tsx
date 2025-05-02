f3f5af8fd37d0fc3e07f1a4389831369e0c72fec
Astride <astridesmn@gmail.com>
2025-05-02 18:25:27 +1100

    fix button.tsx

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,8 +1,8 @@
 import { cn } from "../../lib/utils";
 import { forwardRef } from "react";
+import type { ButtonHTMLAttributes } from "react";
 
-export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     className,
@@ -12,10 +12,16 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     ...props
 }, ref) => {
     return (
-        <button disabled={disabled} ref={ref} {...props} className={cn(`w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition ${className}`)}>
+        <button
+            disabled={disabled}
+            ref={ref}
+            type={type}
+            {...props}
+            className={cn(`w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition ${className}`)}
+        >
             {children}
         </button>
-    )
+    );
 });
 
 Button.displayName = 'Button';
